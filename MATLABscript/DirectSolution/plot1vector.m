99de03e4f623a16cffc0fee0162f1c9ad1f130ff
Andrew Song <songandr@usc.edu>
2024-12-06 13:31:13 -0800

    Update plot1vector.m
    Updated for x in R2 compatibility and added 3 test cases passed as string arguments.

--- MATLABscript/DirectSolution/plot1vector.m
+++ MATLABscript/DirectSolution/plot1vector.m
@@ -1,4 +1,4 @@
-function plot1vector(vec, name, title1, vis)
+function plot1vector(vec, name, title1, vis, test)
 
 % plots a column vector of 3-dimensional points (x, y, z1, x2, y2,
 % z2....)
@@ -7,22 +7,22 @@ function plot1vector(vec, name, title1, vis)
 % symmetry vectors is off or on
 
 gap = 0.4;
+vec = cell2mat(vec);
+x = zeros(length(vec)/2, 1);
+y = zeros(length(vec)/2, 1);
 
-x = zeros(length(vec)/3, 1);
-y = x;
-z = x;
-
-for i = 1:length(vec)/3
-x(i) = vec(3*i-2);
-y(i) = vec(3*i-1);
-z(i) = vec(3*i);
+for i = 1:length(vec)/2
+x(i) = vec(2*i-1);
+y(i) = vec(2*i);
 labels{i} = [name, num2str(i)];
 end
 
+figure
 title(title1)
 xlabel('x');
 ylabel('y');
 
+hold on
 xlim([min(x) - gap, max(x) + gap]);
 ylim([min(y) - gap, max(y) + gap]);
 plot(x, y);
@@ -33,19 +33,34 @@ end
 
 
 % Use the plot function to draw the horizontal line
-plot([x(1), x(2)], [y(1), y(2)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(12), x(9)], [y(12), y(9)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(3), x(6)], [y(3), y(6)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(7), x(8)], [y(7), y(8)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(12), x(1)], [y(12), y(1)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(12), x(3)], [y(12), y(3)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
-plot([x(6), x(9)], [y(6), y(9)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+if test == "Miura"
+    plot([x(1), x(6)], [y(1), y(6)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(2), x(5)], [y(2), y(5)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(5), x(8)], [y(5), y(8)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(4), x(9)], [y(4), y(9)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+end
+
+if test == "Folding"
+    plot([x(1), x(6)], [y(1), y(6)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(2), x(5)], [y(2), y(5)],'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+end
+
+if test == "Rotating Squares"
+    plot([x(1), x(2)], [y(1), y(2)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(1), x(12)], [y(1), y(12)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(12), x(3)], [y(12), y(3)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(12), x(9)], [y(12), y(9)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(3), x(6)], [y(3), y(6)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(7), x(8)], [y(7), y(8)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+    plot([x(6), x(9)], [y(6), y(9)], 'Color', [0 0.4470 0.7410], 'LineWidth', 0.5);
+end
 
 for i = 1:length(labels)
     text(x(i), y(i), labels{i}, 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
 end
 
 %visualization of the lattice vectors 
+%{
 if vis == 1
 plot([x(1) - 0.5*gap, x(1) - 0.5*gap], [y(10), y(2)], 'r-', 'LineWidth', 0.5);
 plot([x(1) - 0.3, x(1) - 0.1], [y(10), y(10)], 'r-', 'LineWidth', 0.5);
@@ -58,6 +73,8 @@ plot([x(1), x(1)], [y(1) - 0.3, y(1) - 0.1], 'r-', 'LineWidth', 0.5);
 plot([x(5), x(5)], [y(1) - 0.3, y(1) - 0.1], 'r-', 'LineWidth', 0.5);
 text((x(1)+x(5))/1.5, y(1)-0.5*gap, "E2 = " + num2str(x(5) - x(1)), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right', 'Color', 'r');
 end
+%}
+hold off
 
 end
 
