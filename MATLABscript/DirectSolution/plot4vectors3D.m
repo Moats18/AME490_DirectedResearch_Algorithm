c06e45b5982693fca20bfa0593d9c8f64f4e3472
Andrew Song <songandr@usc.edu>
2024-12-06 13:32:41 -0800

    Update plot4vectors3D.m
    Updated for use where x is always in R2 and y is always in R3.

--- MATLABscript/DirectSolution/plot4vectors3D.m
+++ MATLABscript/DirectSolution/plot4vectors3D.m
@@ -1,4 +1,4 @@
-function plot4vectors3D(vectors, titles, vis)
+function plot4vectors3D(vectors, titles, vis, test)
 
 % plots four column vectors of 3-dimensional points (x1, y1, z1, x2, y2,
 % z2....) separately in 2x2 subplot
@@ -9,8 +9,12 @@ function plot4vectors3D(vectors, titles, vis)
 
 for i = 1:length(titles)
 
-plot1vector3D(vectors(:, i), '', titles{i}, vis);
+    if strcmp(titles{i}, 'Initial Y') || strcmp(titles{i}, 'Final Y')
+        plot1vector3D(vectors(:, i), '', titles{i}, vis, test);
+    end
 
-end
+    if strcmp(titles{i}, 'Initial X') || strcmp(titles{i}, 'Final X')
+        plot1vector(vectors(:, i), '', titles{i}, vis, test);
+    end
 
 end
