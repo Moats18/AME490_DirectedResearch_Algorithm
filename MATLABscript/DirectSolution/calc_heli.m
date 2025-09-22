832957c1ee494b48c3c263e8e37b67664d2a0de2
Andrew Song <songandr@usc.edu>
2025-09-22 15:09:45 -0700

    Update calc_heli.m
    Added function description and clarifying comments.

--- MATLABscript/DirectSolution/calc_heli.m
+++ MATLABscript/DirectSolution/calc_heli.m
@@ -1,17 +1,40 @@
-function [T1, T2, R1, R2] = calc_heli(q, p, theta1, tau1, z, e1)
+function [T1, T2, R1, R2] = calc_heli(p, q, theta1, tau1, z, e)
+% 
+% returns the operators parameterizing a screw isometry generating the
+% helical group
+%
+% inputs:
+% (p, q): a pair of integers satisfying the discreteness condition p*tau1 +
+% q*tau2 = 0, p*theta1 + q*theta2 = 2*pi.
+% theta1: the angle parameterizing the rotation associated with the screw
+% isometry used to generate the helical group
+% tau1: the translation in R3 associated with the screw isometry used to generate the helical group 
+% z: a vector in R3 orthogonal to z parameterizing the origin of the isometry
+% e: a unit vector in R3 parameterizing the rotation axis
+%
+% outputs:
+% T1: the translation operator in R3 used in g1
+% T2: the translation operator in R3 used in g2
+% R1: the rotation operator in SO(3) used in g1
+% R2: the rotation operator in SO(3) used in g2
 
-tau2 = -p/q*tau1;
-theta2 = 2*pi/q - p/q*theta1;
-I = eye(3,3);
+I = eye(3,3); % working in R3
 
-% Rotation matrices where e1 is the z-axis
-R1 = [cos(theta1), -sin(theta1), 0; sin(theta1), cos(theta1), 0; 0, 0, 1];
-R2 = [cos(theta2), -sin(theta2), 0; sin(theta2), cos(theta2), 0; 0, 0, 1];
+% enforcing the discreteness conditions
+tau2 = -p/q*tau1; % p*tau1 = - q*tau2
+theta2 = (2*pi - p*theta1)/q; % p*theta1 + q*theta2 = 2*pi
 
-% Translation vectors
-T1 = tau1 * e1 + (I - R1) * z;
-T2 = tau2 * e1 + (I - R2) * z;
+% Rotation matrices where e is the z-axis
+R1 = [cos(theta1), -sin(theta1), 0; 
+      sin(theta1), cos(theta1), 0;
+      0, 0, 1];
+R2 = [cos(theta2), -sin(theta2), 0;
+      sin(theta2), cos(theta2), 0;
+      0, 0, 1];
 
+% Translation operators
+T1 = tau1 * e + (I - R1) * z;
+T2 = tau2 * e + (I - R2) * z;
 
 end
 
